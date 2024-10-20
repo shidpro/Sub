aaf4fe27114c6acfc34a896fdb682f33d0224cfa
shidpro <katiyajunesh56567@gmail.com>
2024-10-20 11:48:08 +0000

    Pending changes exported from your codespace

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ cat <<EOL > .devcontainer/devcontainer.json
             "version": "latest"
         }
     },
-    "postStartCommand": "python3 /workspaces/sahil/sahil.py",
+    "postStartCommand": "python3 /workspaces/Sub/sahil.py",
     "customizations": {
         "vscode": {
             "settings": {
